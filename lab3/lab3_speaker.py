f1942d5185f6ac01fcf1345623bcc4bbf2c6f923
Jeffrey <jeffreywjlim@gmail.com>
2025-02-07 18:56:00 +0000

    Speaker part done

---
+++ lab3/lab3_speaker.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Module to play sounds when the touch sensor is pressed.
+This file must be run on the robot.
+"""
+ 
+from utils import sound
+from utils.brick import TouchSensor, wait_ready_sensors, reset_brick
+
+C4 = sound.Sound(duration=0.3, pitch="C4", volume=100)
+E4 = sound.Sound(duration=0.3, pitch="E4", volume=100)
+G4 = sound.Sound(duration=0.3, pitch="G4", volume=100)
+B4 = sound.Sound(duration=0.3, pitch="B4", volume=100)
+
+wait_ready_sensors() # Note: Touch sensors actually have no initialization time
+
+
+def play_sound(x):
+    if x==1:
+        C4.play()
+    elif x==2:
+        E4.play()
+    elif x==3:
+        G4.play()
+    elif x==4:
+        B4.play()
+    else:
+        print("err: invalid note")
+
+if __name__=='__main__':
+    
+    while(1):
+        x = input("enter note # (1,2,3,4): ")
+        play_sound(int(x))
+        
---
+++ lab3/lab3_triple_input.py
@@ -0,0 +1,11 @@
+'''
+This file contains the code for user to our music device
+using input from 3 tactile switches.
+'''
+
+from utils.brick import TouchSensor, reset_brick
+TS1 = TouchSensor(
+TS1 = TouchSensor(
+TS1 = TouchSensor(
+
+
