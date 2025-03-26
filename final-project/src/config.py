ccacce0d480f580a558679454e8b2efddbe7c812
Lucia-Cai <140670562+Lucia-Cai@users.noreply.github.com>
2025-03-26 13:16:51 -0400

    Update config.py

--- final-project/src/config.py
+++ final-project/src/config.py
@@ -1,15 +1,15 @@
 # config.py
 
 # Sensor Ports
-COLOR_SENSOR_PORT = '1'  # Port for the color sensor
-ULTRASONIC_REAR_PORT = '2'  # South-facing ultrasonic sensor
-ULTRASONIC_SIDE_PORT = '3'  # East-facing ultrasonic sensor
+ULTRASONIC_SIDE_PORT = '1'  # X ultrasonic sensor 
+ULTRASONIC_REAR_PORT = '2'  # Y ultrasonic sensor
+COLOR_SENSOR_PORT = '3'  # Port for the color sensor
 TOUCH_SENSOR_PORT = '4'  # Port for the touch sensor (emergency stop)
 
 # Motor Ports
-LEFT_MOTOR_PORT = 'A'  # Port for the left drive motor
-RIGHT_MOTOR_PORT = 'B'  # Port for the right drive motor
-SANDBAG_MOTOR_PORT = 'C'  # Port for the sandbag deployment motor
+LEFT_MOTOR_PORT = 'B'  # Port for the left drive motor
+RIGHT_MOTOR_PORT = 'C'  # Port for the right drive motor
+SANDBAG_MOTOR_PORT = 'A'  # Port for the sandbag deployment motor
 
 # Physical Constants
 WHEEL_RADIUS = 2.8  # Wheel radius in cm
