925fdd58dc4e28920ccc5a460394859bbc10b749
jrogan <jamesrogan53@gmail.com>
2025-03-27 20:16:32 +0000

    Added dropping to sweeping

--- final-project/src/sweep.py
+++ final-project/src/sweep.py
@@ -5,24 +5,38 @@ import color_sensor
 
 SWEEP_MOTOR = Motor("B")
 wait_ready_sensors(True)
-SWEEP_MOTOR.set_limits(30, 360)
-
+SWEEP_MOTOR.set_limits(15, 360)
+SWEEP_MOTOR.reset_encoder()
 
 def reset_sweep_position(motor: Motor):
     motor.set_position(0)
-    
+    time.sleep(2)
 
 def sweep(motor: Motor):
     reset_sweep_position(motor)
-    motor.set_position(100)
-    motor.get_position
+    motor.set_position(80)
+    time.sleep(3)
+    motor.set_position(-80)
+    time.sleep(3)
+    motor.set_position(0)
+    time.sleep(3)
     
     
 if __name__ == "__main__":
-    sweep_motor_thread = threading.Thread(target=sweep, args=(SWEEP_MOTOR))
+    sweep_motor_thread = threading.Thread(target=sweep, args=(SWEEP_MOTOR, ))
+    sweep_motor_thread.start()
     detector = color_sensor.ColorDetector()
-    for i in range(20):
+    color = None
+    while color != "red":
         color = detector.detect_color()
         print(color)
-    
-    
+        time.sleep(0.1)
+    pos = SWEEP_MOTOR.get_position()
+    print(pos)
+    sweep_motor_thread.join()
+    print(pos)
+    time.sleep(2)
+    SWEEP_MOTOR.set_position(pos)
+    # Drop block here
+    time.sleep(5)
+    SWEEP_MOTOR.set_position(0)
